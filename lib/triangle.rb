b5052f9af92aa7733a9c3bd5198458f12eae5b61
Dan Hintz <dhintz@live.com>
2018-09-08 22:52:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,12 @@ class Triangle
   
   def kind
     dimensions = [@a_size, @b_size, @c_size]
-    if dimensions.all? == @a_size 
+    if dimensions.uniq.length == 3 
       :equilateral
-    elsif 
+    elsif dimensions.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
