38e2f5528ca39a8079ea6b8e90062362d3355d6e
Dan Hintz <dhintz@live.com>
2018-09-08 23:12:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,12 @@ class Triangle
   end
   
   def kind
-    if @a_size + @b_size < @c_size || @a_size + @c_size < @b_size || @b_size + @c_size < @a_size || dimensions.any?(0)
-      
     dimensions = [@a_size, @b_size, @c_size]
+    if @a_size + @b_size < @c_size || @a_size + @c_size < @b_size || @b_size + @c_size < @a_size || dimensions.any? {|i| i <= 0}
+      
+      
+      
+
     if dimensions.uniq.length == 1 
       :equilateral
     elsif dimensions.uniq.length == 2
