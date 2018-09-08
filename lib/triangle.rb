f1d9333c11d7f760e4e337732bc750605a0d0026
Dan Hintz <dhintz@live.com>
2018-09-08 22:48:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
+  attr_accessor :a_size,  :b_size, :c_size
   
+  def initialize(a, b, c)
+    @a_size = a
+    @b_size = b
+    @c_size = c
+  end
+  
+  def kind
+    dimensions = [@a_size, @b_size, @c_size]
+    if dimensions.all? == @a_size 
+      :equilateral
+    elsif 
 end
